e13c17527d21498211b5afbbc25115c1bf3f8b0c
Rabinson Shrestha <rabinshrestha4100@gmail.com>
2020-12-14 21:36:47 -0600

    added header section to the website

--- BattleShip/src/app/app.module.ts
+++ BattleShip/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BoardOneComponent } from './components/board-one/board-one.component';
+import { BoardTwoComponent } from './components/board-two/board-two.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BoardOneComponent,
+    BoardTwoComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
